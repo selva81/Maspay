49ec983f527fc75e4b25b7037359800edb68fe57
root <root@server.example.com>
2018-07-07 17:07:39 +0530

    This is the first push for the Maspay application - Version 1

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>Chennai contact details</h2>";
+echo "Payilagam<br>";
+echo "Velachery, 1st main road!<br>";
+echo "Chennai", "tamilnadu ", "600042 ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Welcome to the Maspay application login page</h1>
+
+<?php
+echo "Chennai, Tamilnadu";
+?> 
+
+</body>
+</html>
